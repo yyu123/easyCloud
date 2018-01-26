7265095ec0c7be427ccc8405007fc7a2c53a720c
yuzhijian <yuxia2217@163.com>
2018-01-26 11:22:10 +0800

    test

---
+++ easy-cloud/src/main/java/com/rongyi/EasyCloudApplication.java
@@ -0,0 +1,12 @@
+package com.rongyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCloudApplication.class, args);
+	}
+}
